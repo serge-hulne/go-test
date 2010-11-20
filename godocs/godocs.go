a9a2e5d1cc1805738e1df5a2f71baea8cef2c87a
Serge Hulne <serge.hulne@gmail.com>
2010-11-20 09:32:01 +0100

    Added pause of 1 second to allow the server to start

--- godocs/godocs.go
+++ godocs/godocs.go
@@ -5,10 +5,23 @@ import (
     "fmt"
     "syscall"
     "os"
-    //"strconv"
+    "strconv"
+    "time"
 )
 
+
+func close_server(pid int) {
+    argv3 := []string{"", strconv.Itoa(pid)}
+    cmd3, _ := exec.LookPath("kill")
+    err3 := syscall.Exec(cmd3, argv3, nil)
+    if err3 != 0 {
+        println("warning: godoc process still running in background !")
+    }
+}
+
+
 func main () {
+
     cmd, err := exec.LookPath("godoc")
     //fmt.Printf("path=%v, err=%v\n", cmd, err)
     if err != nil {
@@ -16,9 +29,19 @@ func main () {
     }
     argv := []string{"", "-http=:8090"}
 
+
     //starting server 
     pid, err1 := os.ForkExec(cmd, argv, nil, "", nil)
-    fmt.Printf("err=%v, pid=%v\n", err1, pid)
+    fmt.Printf("err1=%v, pid=%v\n", err1, pid)
+
+    //pause to allow the server to start
+    time.Sleep(1e9)
+    /*
+    *TODO: replace the pause by a test : "Is the server responding"
+    */
+
+    //deferred closing of the server.
+    defer close_server(pid)
 
     //starting browser
     cmd2, err1 := exec.LookPath("links")
@@ -27,19 +50,10 @@ func main () {
         println("- Linux: apt-get install links")
         println("- Mac: Fink install links\n")
     }
+
     argv2 := []string{"", "http://localhost:8090"}
     err2 := syscall.Exec(cmd2, argv2, nil)
-    fmt.Printf("err=%v\n", err2)
-
-    /*
-    argv3 := []string{"", strconv.Itoa(pid)}
-    cmd3, _ := exec.LookPath("kill")
-    err3 := syscall.Exec(cmd3, argv3, nil)
-
-    if err3 != nil {
-        println("warning: godoc process still running in background !") 
-    }
-    */
+    fmt.Printf("err2=%v\n", err2)
 
 }
 
