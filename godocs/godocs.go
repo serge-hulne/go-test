49dd7fc8a5860c84395e90f3383aba591b243b2a
Serge Hulne <serge.hulne@gmail.com>
2010-11-22 10:16:05 +0100

    initial delay fixed

--- godocs/godocs.go
+++ godocs/godocs.go
@@ -7,13 +7,23 @@ import (
     "os"
     "strconv"
     "time"
+    "bufio"
 )
 
+var f *os.File
+var w *bufio.Writer
 
 func close_server(pid int) {
     argv3 := []string{"", strconv.Itoa(pid)}
     cmd3, _ := exec.LookPath("kill")
     err3 := syscall.Exec(cmd3, argv3, nil)
+    fmt.Printf("pid=%v, err3=%v", pid, err3)
+
+    msg :=  "err3=" + strconv.Itoa(err3) + "\n"
+    w.WriteString(msg)
+    w.WriteString("Done\n")
+    w.Flush()
+
     if err3 != 0 {
         println("warning: godoc process still running in background !")
     }
@@ -22,7 +32,19 @@ func close_server(pid int) {
 
 func main () {
 
+    /*
+    *TODO:
+    *   1) replace the pause by a test to check if the server responding
+    *   2) kill server process when the browser exits: Use system calls to monitor the borowser's process  
+    */
+
+    f, _ = os.Open("log.txt",  os.O_WRONLY | os.O_CREAT,  0666)
+    w = bufio.NewWriter(f)
+    w.WriteString("testing log !\n")
+    w.Flush()
     cmd, err := exec.LookPath("godoc")
+    defer f.Close()
+
     //fmt.Printf("path=%v, err=%v\n", cmd, err)
     if err != nil {
         println("Please check that godoc is installed\n")
@@ -33,17 +55,10 @@ func main () {
     //starting server 
     pid, err1 := os.ForkExec(cmd, argv, nil, "", nil)
     fmt.Printf("err1=%v, pid=%v\n", err1, pid)
-
-    //pause to allow the server to start
     time.Sleep(1e9)
-    /*
-    *TODO: replace the pause by a test : "Is the server responding"
-    */
-
-    //deferred closing of the server.
-    defer close_server(pid)
 
     //starting browser
+
     cmd2, err1 := exec.LookPath("links")
     if err1 != nil {
         println("In order to run godocs, links needs to be installed")
@@ -52,8 +67,12 @@ func main () {
     }
 
     argv2 := []string{"", "http://localhost:8090"}
-    err2 := syscall.Exec(cmd2, argv2, nil)
+    //err2 := syscall.Exec(cmd2, argv2, nil)
+     _, err2 := os.ForkExec(cmd2, argv2, nil, "", nil)
     fmt.Printf("err2=%v\n", err2)
+
+    //closing the server.
+    //defer close_server(pid)
 
 }
 
